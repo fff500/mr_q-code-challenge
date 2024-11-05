5400d71685e317e16b2b72c94a7cf581709eea51
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-11-05 11:33:46 +0000

    Trend image was added to symbol card header

--- frontend/src/components/SymbolCard/src/SymbolCardHeader.tsx
+++ frontend/src/components/SymbolCard/src/SymbolCardHeader.tsx
@@ -1,14 +1,29 @@
 import './SymbolCardHeader.css';
+import arrowDown from '@/assets/down.png';
+import arrowUp from '@/assets/up.png';
+import { PriceTrend } from '@/store/stocksSlice';
 
 type SymbolCardHeaderProps = {
   id: string;
-  trend: string | null;
+  trend: PriceTrend | null;
+};
+
+const trendIcons = {
+  [PriceTrend.Up]: {
+    src: arrowUp,
+    alt: `${PriceTrend.Up} arrow`
+  },
+  [PriceTrend.Down]: {
+    src: arrowDown,
+    alt: `${PriceTrend.Down} arrow`
+  }
 };
 
 const SymbolCardHeader = ({ id, trend }: SymbolCardHeaderProps) => {
   return (
     <header className="symbolCard__header">
-      {id} - {trend}
+      <span>{id}</span>
+      {trend && <img src={trendIcons[trend].src} alt={trendIcons[trend].alt} />}
     </header>
   );
 };
